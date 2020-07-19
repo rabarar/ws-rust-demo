68ced042e9026978e96586c37a88f931c6ec43d1
Rob Baruch <robertbaruch@mac.com>
2020-07-18 21:39:31 -0400

    refactored to use wss with TLS

--- README.md
+++ README.md
@@ -4,13 +4,35 @@
 
 start the server
 
-```rust
+```
 $ ./target/release/ws
 ```
 
 start the client
 
-```rust
+```
 $ websocat ws://localhost:9001
 ```
 
+
+### using wss
+
+Generate cert/key
+
+use Common Name = 127.0.0.1
+
+```
+$ openssl req -x509 -sha256 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365'
+```
+
+Generate pkcs12
+```
+$ openssl pkcs12 -export -out identity.pfx -inkey key.pem -in cert.pem
+```
+
+Add the indentity to the Keychain and manually mark it as trusted
+
+start the client
+```
+$ websocat wss://127.0.0.1:8443
+```
--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,87 @@ use tungstenite::protocol::frame::coding::CloseCode::Normal;
 use tungstenite::protocol::CloseFrame;
 use tungstenite::server::accept;
 
+use std::io::{Read};
+use std::fs::File;
+use std::sync::Arc;
+use native_tls::{Identity, TlsAcceptor};
+
 fn main() {
-    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
-    for stream in server.incoming() {
+
+    let mut file = File::open("identity.pfx").unwrap();
+    let mut identity = vec![];
+    file.read_to_end(&mut identity).unwrap();
+    let identity = Identity::from_pkcs12(&identity, "hunter2").unwrap();
+
+    let acceptor = TlsAcceptor::new(identity).unwrap();
+    let acceptor = Arc::new(acceptor);
+
+    let listener = TcpListener::bind("127.0.0.1:8443").unwrap();
+
+
+
+    for stream in listener.incoming() {
         println!("received incoming connection...");
-        spawn(move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
 
-            loop {
+        match stream {
+            Ok(stream) => {
+                let acceptor = acceptor.clone();
+                spawn(move || {
+                    let stream = acceptor.accept(stream).unwrap();
+                    let mut websocket = accept(stream).unwrap();
 
-                let msg = match websocket.read_message() {
-                    Err(why) => {
-                        println!("read_message, reason: {:?}", why);
-                        break;
-                    }
-                    Ok(msg) => msg,
-                };
+                    loop {
 
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    match msg.to_text().unwrap().trim_end() {
-                        "quit" => {
-                            println!("quitting");
-                            let cf = CloseFrame {
-                                code: Normal,
-                                reason: std::borrow::Cow::Borrowed("bye"),
-                            };
-
-                            println!("closing...");
-                            match websocket.close(Option::Some(cf)) {
-                                Err(why) => panic!("{:?}", why),
-                                Ok(_) => (),
+                        let msg = match websocket.read_message() {
+                            Err(why) => {
+                                println!("read_message, reason: {:?}", why);
+                                break;
                             }
+                            Ok(msg) => msg,
+                        };
 
-                            match websocket.write_pending() {
-                                Err(why) => panic!("{:?}", why),
-                                Ok(rc) => println!("closed: {:?}...", rc),
+                        // We do not want to send back ping/pong messages.
+                        if msg.is_binary() || msg.is_text() {
+                            match msg.to_text().unwrap().trim_end() {
+                                "quit" => {
+                                    println!("quitting");
+                                    let cf = CloseFrame {
+                                        code: Normal,
+                                        reason: std::borrow::Cow::Borrowed("bye"),
+                                    };
+
+                                    println!("closing...");
+                                    match websocket.close(Option::Some(cf)) {
+                                        Err(why) => panic!("{:?}", why),
+                                        Ok(_) => (),
+                                    }
+
+                                    match websocket.write_pending() {
+                                        Err(why) => panic!("{:?}", why),
+                                        Ok(rc) => println!("closed: {:?}...", rc),
+                                    }
+
+                                    break;
+                                }
+                                _ => {
+                                    let resp = tungstenite::protocol::Message::Text(
+                                        "hi there: ".to_string() + msg.to_text().unwrap(),
+                                    );
+
+                                    match websocket.write_message(resp) {
+                                        Err(why) => panic!("{:?}", why),
+                                        Ok(_) => (),
+                                    }
+                                }
                             }
-
-                            break;
                         }
-                        _ => {
-                            let resp = tungstenite::protocol::Message::Text(
-                                "hi there: ".to_string() + msg.to_text().unwrap(),
-                            );
-
-                            match websocket.write_message(resp) {
-                                Err(why) => panic!("{:?}", why),
-                                Ok(_) => (),
-                            }
-                        }
                     }
-                }
+                    println!("terminating thread...");
+                });
+
             }
-            println!("terminating thread...");
-        });
+            Err(_e) => { /* connection failed */ }
+        }
     }
 }
 
